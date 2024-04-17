497496471bb727980db1d05226cd6b3603b4b2d6
mjoes <morten.slingsby@gmail.com>
2024-04-17 14:17:52 +0200

    Working FM lookup

--- drums/fm_hit.h
+++ drums/fm_hit.h
@@ -8,8 +8,7 @@
 using namespace std;
 
 struct FmHitSculpt {
-    float fm_amount_, velocity_;
-    uint16_t frequency_;    
+    uint16_t frequency_, velocity_, fm_amount_;
     uint8_t ratio_[2];
 };
 
@@ -24,6 +23,8 @@ public:
         dis(-1,1)
         {
             rel_pos_ = 0;
+            samplesPerCycle = 1024;
+            bitsSine = 10;
         }
     ~FmHit() {}
 
@@ -43,32 +44,36 @@ public:
 
     void set_frequency(uint8_t frequency) {
         FM.frequency_ = frequency / 3 + 60; // range between 60 and 90
+        tW_ = 4294967296 / sample_rate_ * 440;//FM.frequency_;
     }
 
     void set_velocity(uint16_t velocity, bool accent) {
         if (accent == true) {
-            FM.velocity_ = 1.0;
+            FM.velocity_ = 1000;
         } else {
-            FM.velocity_ = velocity / 1000.0;
+            FM.velocity_ = velocity;
         }
     }
 
     void set_ratio(uint8_t ratio) {
-        uint8_t normalized_ratio = (ratio * 3.5 + 150) / 2; // range between 75 and 250
-        FM.ratio_[0] = (3 * normalized_ratio / 7);
-        FM.ratio_[1] = normalized_ratio;
+        uint8_t normalized_ratio = (ratio * 7 / 2 + 150) / 2; // range between 75 and 250
+        FM.ratio_[0] = (3 * normalized_ratio * FM.frequency_ / 7);
+        FM.ratio_[1] = normalized_ratio * FM.frequency_;
     }
 
     void set_fm_amount(uint16_t fm_amount, uint8_t fm_type_prb) {
         if (bernoulli_draw(fm_type_prb - 40) == 0) { // Tunes how often it is overdriven
-            FM.fm_amount_ = fm_amount / 100.0;
+            FM.fm_amount_ = fm_amount;
         } else { // bonkers mode
-            FM.fm_amount_ = fm_amount / 1.0;
+            FM.fm_amount_ = fm_amount * 100;
         }
+        amp_ratio_ = (FM.fm_amount_ / 100.0f) * (1.0f / 65535.0f); // Uniform for nows
+        amp_ratio_s = (FM.fm_amount_ << 15) / (100 * 65535);
     }
 
     void set_start(uint8_t pattern_nr, uint8_t random_pattern_nr, uint8_t randomness, bool accent) {
         rel_pos_ = 0;
+        sampleNr = 0;
         running_ = true;
         end_i_ = length_decay_;
         set_velocity(300, accent);
@@ -81,18 +86,18 @@ public:
             return 0;
 
         int32_t sample;
-        float t = static_cast<float>(rel_pos_) / sample_rate_;
-        float rel_env = interpolate_env_old(rel_pos_, length_decay_, exp_env);
-        sample = GenerateSample(t, rel_env);
-        sample *= FM.velocity_;
-        sample *= rel_env;
-        int16_t output = sample;
+        uint16_t rel_env = interpolate_env(rel_pos_, length_decay_, exp_env);
+        sample = GenerateSample(rel_env);
+        // int32_t nan = GenerateSample_old(rel_env);
+        sample = sample * FM.velocity_ / 1000;
+        sample *= rel_env / 2;
+        int16_t output = sample / 32767;
 
         rel_pos_ += 1;
         if (rel_pos_ >= end_i_) {
             running_ = false;
         }
-        return output;           
+        return output;
     }
 
 private:
@@ -101,20 +106,117 @@ private:
     bool running_;
     mt19937& gen_;
     FmHitSculpt FM;
+    // float dt = 1.0f / sample_rate_;
+    // float sample_f, angle;
+    // float period = sample_rate_ / FM.frequency_;
+
+    float angle, amp_ratio_;
+    uint32_t amp_ratio_s;
+    uint16_t sample_N;
+    uint32_t sampleNr;
+    uint16_t samplesPerCycle;
+    uint32_t phase_acc, tW_;
+    uint8_t bitsSine;
+    uint16_t phase_inc;
 
     uniform_int_distribution<int32_t> dis;
 
-    int32_t GenerateSample(float t, float rel_env) {
-        float amp_ratio_ = FM.fm_amount_ * rel_env; // Uniform for nows
-        float mod_1 = amp_ratio_ * sin(2 * M_PI * (FM.ratio_[0] * FM.frequency_) * t);
-        float mod_2 = amp_ratio_ * sin(2 * M_PI * (FM.ratio_[1] * FM.frequency_) * t);
-        float mod_3 = dis(gen_) * interpolate_env_old(rel_pos_, 3480, exp_env); // Whitenoise transient
+    inline int16_t LookupFM(uint16_t frequency) {
+        uint8_t shift = 16;
+        uint32_t s1 = (frequency << shift) / sample_rate_;
+        uint32_t s2 = rel_pos_ * s1;
+        uint16_t int_pos = ((s2 & ((1 << shift) - 1)) * 1024 / (1 << shift));
+        int16_t a = sine[int_pos];
+        int16_t b = sine[int_pos + 1];
+        int16_t base_sample = a + (b - a) * (s2 & ((1 << shift) - 1) >> shift);
 
-        int32_t sample = 32767 * sin(2 * M_PI * FM.frequency_ * t + mod_1 + mod_2);// + mod_3);
-        
-        return sample;
+        return base_sample;
     }
 
+    int16_t GenerateSample(uint16_t rel_env) {
+        phase_acc += tW_;
+        phase_inc = phase_acc >> (32 - bitsSine);
+        int32_t fraction_fp = (phase_acc & ((1 << (32 - bitsSine)) - 1));
+        int16_t a = sine[phase_inc];
+        int16_t b = sine[phase_inc + 1];
+        int16_t base_sample;
+        if ((b-a) < 0) {
+            base_sample = a - (((a - b) * fraction_fp) >> 22);
+        } else {
+            base_sample = a + (((b - a) * fraction_fp) >> 22);
+        }
 
+        float fraction = static_cast<float>(phase_acc & ((1 << (32 - bitsSine)) - 1)) / (1 << 22);  // Fractional part of phase increment
+        int16_t base_sample_2 = a + (b - a) * (fraction);
+        int16_t base_sample_3 = a + ((b - a) * (fraction_fp) >> 22);
+        int16_t base_sample_4 = a + ((b - a) * (phase_acc -(phase_inc<<22))>>22);
+        //printf("%i, %i, %i, %i, %i, %i  \n", a, b, base_sample, base_sample_2, base_sample_3 ,base_sample_4);
+        
+        return base_sample_3;
+    }
 };
 
+// static inline signed short fast_interpolate(unsigned short x) {
+//         register unsigned short i = (x >> 8);
+//         return lut[i] + (signed short)((signed long)(lut[i + 1] - lut[i]) * (x - (i << 8)) >> 8);
+// }
+
+
+
+
+
+
+
+    
+//  int32_t GenerateSample(uint16_t rel_env) {
+//         int16_t mod_1 = LookupFM(FM.ratio_[0]);
+//         int16_t mod_2 = LookupFM(FM.ratio_[1]);
+//         // int64_t total_mod;
+//         // total_mod = ((mod_1 + mod_2) * 10000000 / 32767) * ((rel_env << 15) / 65535);
+//         // total_mod >>= 15;
+//         // // printf("step1: %li \n", total_mod);
+//         // total_mod = total_mod * FM.fm_amount_ / 100; // Total mod factor 1e7 too large
+//         float flt_mod = ((mod_1 + mod_2) / 32767.0f) * (rel_env / 65535.0f) * (FM.fm_amount_ / 100.0f);
+//         int16_t offset = static_cast<int16_t>(flt_mod * 1024);
+        
+//         // uint64_t angle_alt = ((sampleNr * FM.frequency_) << 15)/sample_rate_;
+//         // int64_t total_ang;
+//         // if (total_mod > 0) {
+//         //     total_ang = ((total_mod << 15) / 62831853) + angle_alt;
+//         // } else {
+//         //     total_ang =  angle_alt - ((-total_mod << 15) / 62831853);
+//         // }
+
+
+//         // printf("step2: %li, fm_amount: %i \n", total_mod, FM.fm_amount_);
+//         // printf("mod: %i \n", total_ang >> 15);
+//         // printf ("total_shift: %i,%f\n", (test), ((double)(test & ((1 << 15) - 1))/(1 << 15)));
+
+//         uint64_t angle_alt = ((sampleNr * FM.frequency_) << 15)/sample_rate_;
+//         //  printf ("total_shift: %i,%f\n", (total_ang), ((double)(total_ang & ((1 << 15) - 1))/(1 << 15)));
+//         //  printf ("angle_alt_2: %i,%
+//         , (angle_alt), ((double)(angle_alt & ((1 << 15) - 1))/(1 << 15)));
+
+
+    //     uint16_t int_pos = ((angle_alt & ((1 << 15) - 1)) * 1024 / (1 << 15));
+    //     int16_t int_pos_off = (int_pos + offset);
+    //     // printf("%i, %i, %i \n", int_pos, offset, int_pos_off);
+    //     while (int_pos_off < 0) int_pos_off += 1024;
+    //     while (int_pos_off >= 1023) int_pos_off -= 1024;
+    //     // printf("%i \n", int_pos_off);
+        
+    //     int16_t a = sine[int_pos_off];
+    //     int16_t b = sine[int_pos_off + 1];
+    //     int16_t base_sample = a + (b - a) * (angle_alt & ((1 << 15) - 1) >> 15);
+
+    //     // printf("old: %f  ", angle);
+    //     // printf ("new: %i,%f\n", (int_pos), ((double)(int_pos & ((1 << 15) - 1))/(1 << 15)));
+        
+	// 	int32_t out = base_sample;
+    //     sampleNr++;
+    //     if (sampleNr >= sample_N) sampleNr = 0; 
+
+    //     return out;
+    // }
+
+    
