bb37c29c681f4760dcbbebf26dabc68d29ba665d
mjoes <morten.slingsby@gmail.com>
2024-05-27 21:46:26 +0200

    Fix FM hit

--- utils/envelopes.h
+++ utils/envelopes.h
@@ -9,6 +9,7 @@ extern const uint16_t lambda[];
 extern const int16_t sine[];
 extern const int16_t cosine[];
 extern const int16_t env_overdrive[];
+extern const int16_t beta_inv[];
 
 uint16_t interpolate_env(uint32_t rel_pos_, uint32_t length_decay_, const uint16_t* lookup_table_);
 float interpolate_env_old(uint32_t rel_pos_, uint32_t length_decay_, const uint16_t* lookup_table_);
